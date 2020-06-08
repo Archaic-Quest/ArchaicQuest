2160be61bd4e23254f6d3a32810a2af990b9870a
MaroonShaded <maroonshaded@gmail.com>
2020-06-08 14:57:40 +0100

    Changed max stack sizes of some items
    The items are Sacrificial Knife, Heart, Skull, and Red Clay Jar.

--- src/main/java/com/archaicquest/init/ModItems.java
+++ src/main/java/com/archaicquest/init/ModItems.java
@@ -13,10 +13,10 @@ public class ModItems
 
     public static final RegistryObject<Item> JADE_STONE = ITEMS.register("jade_stone", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> GARNET = ITEMS.register("garnet", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
-    public static final RegistryObject<Item> HEART = ITEMS.register("heart", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
-    public static final RegistryObject<Item> SKULL = ITEMS.register("skull", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
-    public static final RegistryObject<Item> SACRIFICIAL_KNIFE = ITEMS.register("sacrificial_knife", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
-    public static final RegistryObject<Item> RED_CLAY_JAR = ITEMS.register("red_clay_jar", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
+    public static final RegistryObject<Item> HEART = ITEMS.register("heart", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS).maxStackSize(16)));
+    public static final RegistryObject<Item> SKULL = ITEMS.register("skull", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS).maxStackSize(1)));
+    public static final RegistryObject<Item> SACRIFICIAL_KNIFE = ITEMS.register("sacrificial_knife", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS).maxStackSize(1)));
+    public static final RegistryObject<Item> RED_CLAY_JAR = ITEMS.register("red_clay_jar", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS).maxStackSize(1)));
     public static final RegistryObject<Item> AZTEC_CALENDAR = ITEMS.register("aztec_calendar", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> GOLD_SUN_DISK = ITEMS.register("gold_sun_disk", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
 }
