e769e47e754179601210b28a7976072a76fd4012
MaroonShaded <maroonshaded@gmail.com>
2020-06-08 14:47:41 +0100

    Added the item group to the lang file

--- src/main/java/com/archaicquest/ArchaicQuest.java
+++ src/main/java/com/archaicquest/ArchaicQuest.java
@@ -3,6 +3,8 @@ package com.archaicquest;
 import com.archaicquest.init.ModItems;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -13,21 +15,19 @@ public class ArchaicQuest
 {
     public static final String MODID = "archaicquest";
 
+    public static final ItemGroup ARCHAIC_ITEMS = new ItemGroup("archaicQuest")
+    {
+        @OnlyIn(Dist.CLIENT)
+        @Override
+        public ItemStack createIcon() { return new ItemStack(ModItems.JADE_STONE.get()); }
+    }.setTabPath("archaic_quest");
+
     public ArchaicQuest()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Registries
         ModItems.ITEMS.register(modEventBus);
-        // ModBlocks.BLOCKS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-
-    public static final ItemGroup ARCHAIC_ITEMS = new ItemGroup("archaicQuestTab"){
-        @Override
-        public ItemStack createIcon() { return new ItemStack(ModItems.JADE_STONE.get()); }
-    };
-
 }
--- src/main/java/com/archaicquest/init/ModItems.java
+++ src/main/java/com/archaicquest/init/ModItems.java
@@ -2,7 +2,6 @@ package com.archaicquest.init;
 
 import com.archaicquest.ArchaicQuest;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +11,6 @@ public class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ArchaicQuest.MODID);
 
-    // Example Item - Delete comment later
     public static final RegistryObject<Item> JADE_STONE = ITEMS.register("jade_stone", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> GARNET = ITEMS.register("garnet", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> HEART = ITEMS.register("heart", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
@@ -21,5 +19,4 @@ public class ModItems
     public static final RegistryObject<Item> RED_CLAY_JAR = ITEMS.register("red_clay_jar", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> AZTEC_CALENDAR = ITEMS.register("aztec_calendar", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
     public static final RegistryObject<Item> GOLD_SUN_DISK = ITEMS.register("gold_sun_disk", () -> new Item(new Item.Properties().group(ArchaicQuest.ARCHAIC_ITEMS)));
-
 }
